f95d76c563ef6dc883d007cec28702611e33a85d
Ice-San <rotommine@gmail.com>
2022-12-30 01:03:43 +0000

    Added page translate to portugues with onload function

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Translate Portfolio
+
+window.onload = function WindowLoad(event) {
+    if (window.confirm('Click \"OK\" if you want the Website to be in Portuguese!')) {
+        
+    document.getElementById('aboutme').innerText = "Sobre mim";
+    document.getElementById('aboutme-description').innerText = "Algum texto aqui."
+        
+    }
+}
