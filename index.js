48b3c6ca7c844f57bc4d312ae72c580577a9e72f
Ice-San <rotommine@gmail.com>
2022-12-30 18:13:16 +0000

    ruben | adjusts with the Day and Night theme

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ function whiteTheme() {
     document.querySelector('.fa-toggle-on').style.display = "unset";
     document.getElementById('TextThemeEng').innerText = "Night Theme";
     document.getElementById('TextThemePt').innerText = "Tema Preto";
-    document.querySelector('.Webpage-ChangeTheme i').style.color = "black";
-    document.querySelector('.Webpage-ChangeTheme p').style.color = "black";
+    document.querySelector('.Webpage-ChangeTheme i').style.color = "white";
+    document.getElementById('TextThemePt').style.color = "white";
+    document.getElementById('TextThemeEng').style.color = "white";
     document.body.style = "background-color: rgb(27, 27, 27); color: white;";
     document.querySelector('.Webpage-Icon').style = "background-color: rgb(27, 27, 27);";
 
@@ -50,8 +51,9 @@ function blackTheme() {
     document.querySelector('.fa-toggle-on').style.display = "none";
     document.getElementById('TextThemeEng').innerText = "Day Theme";
     document.getElementById('TextThemePt').innerText = "Tema Branco";
-    document.querySelector('.Webpage-ChangeTheme i').style.color = "white";
-    document.querySelector('.Webpage-ChangeTheme p').style.color = "white";
+    document.querySelector('.Webpage-ChangeTheme i').style.color = "black";
+    document.getElementById('TextThemePt').style.color = "black";
+    document.getElementById('TextThemeEng').style.color = "black";
     document.body.style = "background-color: white; color: black;";
     document.querySelector('.Webpage-Icon').style = "background-color: white;";
 
