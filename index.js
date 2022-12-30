fe38048b74d920395fef3b7e99831d41ad45a75d
Ice-San <rotommine@gmail.com>
2022-12-30 16:17:46 +0000

    Move social midia to the bottom of webpage banner

--- index.js
+++ index.js
@@ -32,19 +32,23 @@ window.onload = function WindowLoad(event) {
    ==============================*/
 
 function whiteTheme() {
+
     document.querySelector('.fa-toggle-off').style.display = "none";
     document.querySelector('.fa-toggle-on').style.display = "unset";
     document.getElementById('TextThemeEng').innerText = "Night Theme";
     document.getElementById('TextThemePt').innerText = "Tema Preto";
     document.body.style = "background-color: rgb(27, 27, 27); color: white;";
     document.querySelector('.Webpage-Icon').style = "background-color: rgb(27, 27, 27);";
+
 }
 
 function blackTheme() {
+
     document.querySelector('.fa-toggle-off').style.display = "unset";
     document.querySelector('.fa-toggle-on').style.display = "none";
     document.getElementById('TextThemeEng').innerText = "Day Theme";
     document.getElementById('TextThemePt').innerText = "Tema Branco";
     document.body.style = "background-color: white; color: black;";
     document.querySelector('.Webpage-Icon').style = "background-color: white;";
+    
 }
