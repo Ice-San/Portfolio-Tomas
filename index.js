ac821b6c8468bb1784b53ed89faaaa535701a9c5
Ice-San <rotommine@gmail.com>
2022-12-30 01:58:35 +0000

    Added definitive Multi-Languages on Portfolio!

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
 // Translate Portfolio
 
+const hideEng = document.querySelectorAll('.eng');
+const hidePt = document.querySelectorAll('.pt');
+
 window.onload = function WindowLoad(event) {
     if (window.confirm('Click \"OK\" if you want the Website to be in Portuguese!')) {
+
+        for (let i = 0; i < hideEng.length; i++) {
+            hideEng[i].style.display = "none";
+          }
+
+    } else {
         
-    document.getElementById('aboutme').innerText = "Sobre mim";
-    document.getElementById('aboutme-description').innerText = "Algum texto aqui."
-        
+        for (let i = 0; i < hidePt.length; i++) {
+            hidePt[i].style.display = "none";
+          }
+
     }
 }
