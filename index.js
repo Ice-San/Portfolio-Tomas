cc896260010a108d3b966484f35c4e6e293e0315
DieTower <rodrigonavecosta.1996@gmail.com>
2023-01-01 22:29:09 +0000

    rodrigo | Main page UI, Skills, Images & Pop-up

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-/* ============================== 
+// ==== HTML Elements ====
+const popUpContainer = document.getElementById("pop-up");
+const popUpAboutMeContainer = document.getElementById("pop-up-about-me");
+const popUpChatMeContainer = document.getElementById("pop-up-chat-me");
+const popUpKnowMoreContainer = document.getElementById("pop-up-know-more");
+const popUpCloseButton = document.querySelector(".pop-up-close-button");
+const chatMeButton = document.getElementById("chat-me-button");
+const aboutMeButton = document.getElementById("about-me-button");
+const knowMoreButton = document.getElementById("know-more-button");
 
-          Website Languages
+// ==== Functions ====
+const showPopUp = () => {
+  if (popUpContainer.classList.contains("hidden"))
+    popUpContainer.classList.remove("hidden");
+};
 
-   ==============================*/
+const destroyPopUpContainers = () => {
+  if (!popUpAboutMeContainer.classList.contains("vanish"))
+    popUpAboutMeContainer.classList.add("vanish");
+  if (!popUpChatMeContainer.classList.contains("vanish"))
+    popUpChatMeContainer.classList.add("vanish");
+  if (!popUpKnowMoreContainer.classList.contains("vanish"))
+    popUpKnowMoreContainer.classList.add("vanish");
+};
 
-const hideEng = document.querySelectorAll('.eng');
-const hidePt = document.querySelectorAll('.pt');
+const hiddenPopUp = () => {
+  destroyPopUpContainers();
+  if (!popUpContainer.classList.contains("hidden"))
+    popUpContainer.classList.add("hidden");
+};
 
-window.onload = function WindowLoad(event) {
-    if (window.confirm('Click \"OK\" if you want the Website to be in Portuguese!')) {
+const showAboutMe = () => {
+  destroyPopUpContainers();
+  showPopUp();
+  popUpAboutMeContainer.classList.remove("vanish");
+};
 
-        for (let i = 0; i < hideEng.length; i++) {
-            hideEng[i].style.display = "none";
-            hidePt[i].style.display = "unset";
-          }
+const showChatMe = () => {
+  destroyPopUpContainers();
+  showPopUp();
+  popUpChatMeContainer.classList.remove("vanish");
+};
 
-    } else {
-        
-        for (let i = 0; i < hidePt.length; i++) {
-            hidePt[i].style.display = "none";
-            hideEng[i].style.display = "unset";
-          }
+const showKnowMe = () => {
+  destroyPopUpContainers();
+  showPopUp();
+  popUpKnowMoreContainer.classList.remove("vanish");
+};
 
-    }
-}
-
-/* ============================== 
-
-          Website Theme
-
-   ==============================*/
-
-function whiteTheme() {
-
-    document.querySelector('.fa-toggle-off').style.display = "none";
-    document.querySelector('.fa-toggle-on').style.display = "unset";
-    document.getElementById('TextThemeEng').innerText = "Night Theme";
-    document.getElementById('TextThemePt').innerText = "Tema Preto";
-    document.body.style = "background-color: rgb(27, 27, 27); color: white;";
-    document.querySelector('.Webpage-Icon').style = "background-color: rgb(27, 27, 27);";
-
-}
-
-function blackTheme() {
-
-    document.querySelector('.fa-toggle-off').style.display = "unset";
-    document.querySelector('.fa-toggle-on').style.display = "none";
-    document.getElementById('TextThemeEng').innerText = "Day Theme";
-    document.getElementById('TextThemePt').innerText = "Tema Branco";
-    document.body.style = "background-color: white; color: black;";
-    document.querySelector('.Webpage-Icon').style = "background-color: white;";
-    
-}
+// ==== Events ====
+popUpCloseButton.addEventListener("click", hiddenPopUp);
+aboutMeButton.addEventListener("click", showAboutMe);
+chatMeButton.addEventListener("click", showChatMe);
+knowMoreButton.addEventListener("click", showKnowMe);
